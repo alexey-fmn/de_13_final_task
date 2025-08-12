7284a1e913c65158c69849fa73a0ab11c46732a1
alexey-fmn <zooyork74@yandex.ru>
2025-08-12 16:42:13 +0300

    feat: выполнены все задачи

--- purchase.py
+++ purchase.py
@@ -5,12 +5,50 @@ purchases = [
     {"item": "bread", "category": "bakery", "price": 2.0, "quantity": 3},
 ]
 
-dict_len = len(purchases)
-revenue = 0
+categories = {}
+max_price_value = 1.0
+category_prices = {}
+category_quantity = {}
 
-for i in range(dict_len):
-    quant = purchases[i]["quantity"]
-    price = purchases[i]["price"]
-    revenue += price * quant
+revenue = sum(p["price"] * p["quantity"] for p in purchases)
+
+for purchase in purchases:
+    category = purchase["category"]
+    item = purchase["item"]
+    if category not in categories:
+        categories[category] = []
+    if item not in categories[category]:
+        categories[category].append(item)
+
+expensive_items = [p for p in purchases if p["price"] >= max_price_value]
+
+for purchase in purchases:
+    category = purchase["category"]
+    price = purchase["price"]
+    quantity = purchase["quantity"]
+
+    if category not in category_prices:
+        category_prices[category] = {"total_price": 0, "total_quantity": 0}
+
+    category_prices[category]["total_price"] += quantity * price
+    category_prices[category]["total_quantity"] += quantity
+
+average_price = {
+    category: round(data["total_price"] / data["total_quantity"], 2)
+    for category, data in category_prices.items()
+}
+
+for purchase in purchases:
+    category = purchase["category"]
+    quantity = purchase["quantity"]
+    if category not in category_quantity:
+        category_quantity[category] = 0
+    category_quantity[category] += quantity
+
+most_frequent = max(category_quantity, key=category_quantity.get)
 
 print(f'Общая выручка: {revenue}')
+print(f'Товары по категориям: {categories}')
+print(f'Покупки дороже {max_price_value}: {expensive_items}')
+print(f'Средняя цена по категориям: {average_price}')
+print(f'Категория с наибольшим количеством проданных товаров: {most_frequent}')
